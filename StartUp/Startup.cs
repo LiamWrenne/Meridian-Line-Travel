98a9ec13c285c16691d43382035e57ba8abdb9b1
Liam Wrenne <Liam2.Wrenne@live.uwe.ac.uk>
2021-12-09 10:50:53 +0000

    new

--- StartUp/Startup.cs
+++ StartUp/Startup.cs
@@ -28,10 +28,9 @@ namespace Meridian_Line_Travel
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDatabaseDeveloperPageExceptionFilter();
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
+            services.AddDatabaseDeveloperPageExceptionFilter();
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
